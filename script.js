0eed1686e96a517917fa4b53a26d5fc4c358929a
Kauan <kauan.s.leite@hotmail.com>
2022-04-20 16:18:11 -0300

    3° e 4° Requisito

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 let body = document.body;
-let divColorPalette = document.getElementById('color-palette');
-let divColor = document.createElement('div');
+let pixelBoard = document.getElementById("pixel-board");
+
+
+
+for(let index1 = 0; index1 < 5; index1 += 1){
+    for(let index2 = 1; index2 <= 5; index2 +=1){
+        let pixels = document.createElement("div");
+        pixels.classList.add("pixel");
+        // pixels.innerHTML= index1;
+
+        pixelBoard.appendChild(pixels);
+    } 
+    let br = document.createElement("br");
+    pixelBoard.appendChild(br);
+}
+
+
+
+
+
+
