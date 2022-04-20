a57a2cbd138f5a7ea66ec724e442ca8971acf6e0
Kauan <kauan.s.leite@hotmail.com>
2022-04-20 16:56:39 -0300

    7° Requisito

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let body = document.body;
 let pixelBoard = document.getElementById("pixel-board");
 
+let colorBlack = document.querySelector(".black");
+let colorRed = document.querySelector(".red");
+let colorGreen = document.querySelector(".green");
+let colorYellow = document.querySelector(".yellow");
 
 
 
@@ -17,6 +21,42 @@ for(let index1 = 0; index1 < 5; index1 += 1){
 }
 
 
+function changeBlack(){
+    colorBlack.classList.add("selected");
+    colorRed.classList.remove("selected");
+    colorGreen.classList.remove("selected");
+    colorYellow.classList.remove("selected");
+}
+
+colorBlack.addEventListener("click", changeBlack);
+
+function changeRed(){
+    colorRed.classList.add("selected");
+    colorBlack.classList.remove("selected");
+    colorGreen.classList.remove("selected");
+    colorYellow.classList.remove("selected");
+}
+
+colorRed.addEventListener("click", changeRed);
+
+
+function changeGreen(){
+    colorGreen.classList.add("selected");
+    colorRed.classList.remove("selected");
+    colorBlack.classList.remove("selected");
+    colorYellow.classList.remove("selected");
+}
+
+colorGreen.addEventListener("click", changeGreen);
+
+function changeYellow(){
+    colorYellow.classList.add("selected");
+    colorGreen.classList.remove("selected");
+    colorRed.classList.remove("selected");
+    colorBlack.classList.remove("selected");
+}
+
+colorYellow.addEventListener("click", changeYellow);
 
 
 
