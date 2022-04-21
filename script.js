d3d75f38dfb2a0b47142ce7ea95215df3d3a3577
Kauan <kauan.s.leite@hotmail.com>
2022-04-21 12:11:11 -0300

    Organizando o JS

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
+// Variaveis
 let body = document.body;
 let pixelBoard = document.getElementById("pixel-board");
 
 let colorBlack = document.querySelector(".black");
-let colorRed = document.querySelector(".red");
-let colorGreen = document.querySelector(".green");
-let colorYellow = document.querySelector(".yellow");
-
+let colorOne = document.querySelector(".one");
+let colorTwo = document.querySelector(".two");
+let colorThree = document.querySelector(".three");
 
 let count1 = 0;
 
+
+// Adiciona os Pixels no Quadro de Pixels
 for(let index1 = 1; index1 <= 5; index1 += 1){
     for(let index2 = 1; index2 <= 5; index2 +=1){
         count1 += 1;
         let pixels = document.createElement("div");
         pixels.classList.add("pixel");
         pixels.classList.add(count1);
-        // pixels.classList.add("linha" + index1);
-        // pixels.classList.add("coluna" + index2);
-        // pixels.innerHTML= index1;
-
         pixelBoard.appendChild(pixels);
     } 
     let br = document.createElement("br");
@@ -26,82 +24,64 @@ for(let index1 = 1; index1 <= 5; index1 += 1){
 }
 
 
-
+// Selecionar a Cor na Paletta de Cores
 let colorSelect = "black";
 
 function changeBlack(){
     colorBlack.classList.add("selected");
-    colorRed.classList.remove("selected");
-    colorGreen.classList.remove("selected");
-    colorYellow.classList.remove("selected");
+    colorOne.classList.remove("selected");
+    colorTwo.classList.remove("selected");
+    colorThree.classList.remove("selected");
     colorSelect = "black";
-
 }
+    colorBlack.addEventListener("click", changeBlack);
 
-colorBlack.addEventListener("click", changeBlack);
-
-function changeRed(){
-    colorRed.classList.add("selected");
+function changeOne(){
+    colorOne.classList.add("selected");
     colorBlack.classList.remove("selected");
-    colorGreen.classList.remove("selected");
-    colorYellow.classList.remove("selected");
+    colorTwo.classList.remove("selected");
+    colorThree.classList.remove("selected");
     colorSelect = "red";
-
 }
+    colorOne.addEventListener("click", changeOne);
 
-colorRed.addEventListener("click", changeRed);
-
-
-function changeGreen(){
-    colorGreen.classList.add("selected");
-    colorRed.classList.remove("selected");
+function changeTwo(){
+    colorTwo.classList.add("selected");
+    colorOne.classList.remove("selected");
     colorBlack.classList.remove("selected");
-    colorYellow.classList.remove("selected");
+    colorThree.classList.remove("selected");
     colorSelect = "green";
-
 }
+    colorTwo.addEventListener("click", changeTwo);
 
-colorGreen.addEventListener("click", changeGreen);
-
-function changeYellow(){
-    colorYellow.classList.add("selected");
-    colorGreen.classList.remove("selected");
-    colorRed.classList.remove("selected");
+function changeThree(){
+    colorThree.classList.add("selected");
+    colorTwo.classList.remove("selected");
+    colorOne.classList.remove("selected");
     colorBlack.classList.remove("selected");
     colorSelect = "yellow";
-
 }
-
-colorYellow.addEventListener("click", changeYellow);
-
-let count2 = 1;
+    colorThree.addEventListener("click", changeThree);
 
 
-
-
+// Pinta os Pixels com a Cor Selecionada
 for(let index = 1; index <= count1; index += 1){
-    let pixelN = document.getElementsByClassName(index)[0]
-    function test(){
-        // pixelN.classList.add("paint");
+    let pixelN = document.getElementsByClassName(index)[0];
+
+    function paint(){
         pixelN.style.backgroundColor = colorSelect;
     }
-
-    pixelN.addEventListener("click", test);
+    pixelN.addEventListener("click", paint);
 }
 
 
-
-
+// Limpa as Cores do Quadro de Pixels
 let botao = document.getElementById("clear-board");
-
 for(let index = 1; index <= count1; index += 1){
-    function vasco(){
-    let allPixels = document.getElementsByClassName(index)[0]
+    function removeColor(){
+    let allPixels = document.getElementsByClassName(index)[0];
 
-        // allPixels.classList.remove("paint");
         allPixels.style.backgroundColor = "white";
     }
-
-    botao.addEventListener('click', vasco)
-
+    botao.addEventListener('click', removeColor)
 }
