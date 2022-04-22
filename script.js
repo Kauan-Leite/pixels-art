27a320b9945f172f2744fcc6beb9626ab9957a89
Kauan <kauan.s.leite@hotmail.com>
2022-04-22 12:57:00 -0300

    10°, 11° e 12° Requisito

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Variaveis
-let body = document.body;
-let pixelBoard = document.getElementById("pixel-board");
+let body = document.body;       // Body
+let pixelBoard = document.getElementById("pixel-board");    //  Pixel Board
 
-let colorBlack = document.querySelector(".black");
-let colorOne = document.querySelector(".one");
-let colorTwo = document.querySelector(".two");
-let colorThree = document.querySelector(".three");
+let number = document.getElementById('board-size');
+
+let colorBlack = document.querySelector(".black");      //  Cor Preta
+let colorOne = document.querySelector(".one");      //  1°Cor
+let colorTwo = document.querySelector(".two");      //  2°Cor
+let colorThree = document.querySelector(".three");  //  3°Cor
+
+
+//  Random Color 1
+let randomColor1 = 'rgb('+ Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+colorOne.style.backgroundColor = randomColor1;
+
+//  Random Color 2
+let randomColor2 = 'rgb('+ Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+colorTwo.style.backgroundColor = randomColor2;
+
+//  Random Color 3
+let randomColor3 = 'rgb('+ Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+colorThree.style.backgroundColor = randomColor3;
+
+
+
+
+
 
 let count1 = 0;
 
-
 // Adiciona os Pixels no Quadro de Pixels
+
 for(let index1 = 1; index1 <= 5; index1 += 1){
     for(let index2 = 1; index2 <= 5; index2 +=1){
         count1 += 1;
@@ -20,8 +40,61 @@ for(let index1 = 1; index1 <= 5; index1 += 1){
         pixelBoard.appendChild(pixels);
     } 
     let br = document.createElement("br");
+    br.classList.add("spacePixel");
     pixelBoard.appendChild(br);
 }
+
+
+
+
+
+
+
+function addPixels(){
+
+    if(number.value < 5 && number.value != ''){
+        number.value = 5;
+    }
+    else if(number.value > 50){
+        number.value = 50;
+    }
+
+
+    for(let index1 = 1; index1 <= number.value; index1 += 1){
+        for(let index2 = 1; index2 <= number.value; index2 +=1){
+            count1 += 1;
+            let pixels = document.createElement("div");
+            pixels.classList.add("pixel");
+            pixels.classList.add(count1);
+            pixelBoard.appendChild(pixels);
+        } 
+        let br = document.createElement("br");
+        br.classList.add("spacePixel");
+        pixelBoard.appendChild(br);
+    }
+
+    // Pinta os Pixels com a Cor Selecionada
+    for(let index2 = 1; index2 <= count1; index2 += 1){
+    let pixelN = document.getElementsByClassName(index2)[0];
+
+    function paint(){
+        pixelN.style.backgroundColor = colorSelect;
+    }
+    pixelN.addEventListener("click", paint);
+    }
+
+    // Limpa as Cores do Quadro de Pixels
+    let botao = document.getElementById("clear-board");
+    for(let index3 = 1; index3 <= count1; index3 += 1){
+        function removeColor(){
+        let allPixels = document.getElementsByClassName(index3)[0];
+
+            allPixels.style.backgroundColor = "white";
+        }
+        botao.addEventListener('click', removeColor)
+    }
+}
+addPixels();
 
 
 // Selecionar a Cor na Paletta de Cores
@@ -41,7 +114,7 @@ function changeOne(){
     colorBlack.classList.remove("selected");
     colorTwo.classList.remove("selected");
     colorThree.classList.remove("selected");
-    colorSelect = "red";
+    colorSelect = randomColor1;
 }
     colorOne.addEventListener("click", changeOne);
 
@@ -50,7 +123,7 @@ function changeTwo(){
     colorOne.classList.remove("selected");
     colorBlack.classList.remove("selected");
     colorThree.classList.remove("selected");
-    colorSelect = "green";
+    colorSelect = randomColor2;
 }
     colorTwo.addEventListener("click", changeTwo);
 
@@ -59,29 +132,46 @@ function changeThree(){
     colorTwo.classList.remove("selected");
     colorOne.classList.remove("selected");
     colorBlack.classList.remove("selected");
-    colorSelect = "yellow";
+    colorSelect = randomColor3;
 }
     colorThree.addEventListener("click", changeThree);
 
 
-// Pinta os Pixels com a Cor Selecionada
-for(let index = 1; index <= count1; index += 1){
-    let pixelN = document.getElementsByClassName(index)[0];
 
-    function paint(){
-        pixelN.style.backgroundColor = colorSelect;
+
+
+
+// Testes
+let testeButton = document.getElementById("generate-board");
+
+testeButton.addEventListener('click', button)
+
+function button (){
+
+if(number.value === ''){
+    alert("Board inválido!")
+    number.value = 5;
+}
+
+let countButton = 0;
+if (countButton === 0){
+    removePixels();
+    countButton = 1;
+}
+if(countButton === 1){
+    addPixels();
+    countButton = 0;
+}
+}
+
+function removePixels(){
+    while(pixelBoard.firstChild) {
+        pixelBoard.removeChild(pixelBoard.firstChild);
     }
-    pixelN.addEventListener("click", paint);
+    count1 = 0;
 }
 
 
-// Limpa as Cores do Quadro de Pixels
-let botao = document.getElementById("clear-board");
-for(let index = 1; index <= count1; index += 1){
-    function removeColor(){
-    let allPixels = document.getElementsByClassName(index)[0];
 
-        allPixels.style.backgroundColor = "white";
-    }
-    botao.addEventListener('click', removeColor)
-}
+
+
